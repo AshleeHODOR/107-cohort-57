caac457cb26126a1f8b0210a78efe035ccfa7ca7
AshleeHODOR <hodor1987@yahoo.com>
2025-05-12 20:02:48 -0700

    107 - react

---
+++ online-store/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './pages/Home'
+
+
+function App() {
+  return (
+    <>
+    <h1>HODOR</h1>
+
+    <Home />
+    </>
+  )
+}
+
+
+export default App
---
+++ online-store/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+// Import section
+//import CSS
+import './Home.css'
+
+//Functional componet
+function Home() {
+    return(
+    <div className="Home">
+
+        <div className="Home-text">
+            <h1>Organika (Home.jsx)</h1>
+            <h3>Fresh Food, Healthier Life.</h3>
+        </div>
+
+    </div>
+
+    )
+}
+
+
+//export section
+export default Home //must match function name!
